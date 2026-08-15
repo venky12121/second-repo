cfacf75a2118648b2fa13be1d9c1990aeb8b8525
venky12121 <strangerthingsstart@gmail.com>
2026-08-15 14:59:12 +0530

    code added

---
+++ app.java
@@ -0,0 +1,7 @@
+// Save this file as HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints text to the console window
+        System.out.println("Hello, World!");
+    }
+}
